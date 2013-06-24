74b3e1c294a75663db3088c14d1f89cae2cce2c1
Daniel Smallwood <daniel@cs4.(none)>
2013-06-24 16:22:44 -0400

    Code cleanup on client/fx/*.lua

--- morbusgame/gamemode/client/fx/cl_headbob.lua
+++ morbusgame/gamemode/client/fx/cl_headbob.lua
@@ -1,6 +1,9 @@
-// Morbus - morbus.remscar.com
-// Developed by Remscar
-// and the Morbus dev team
+--[[
+	Morbus - morbus.remscar.com
+	Developed by Remscar
+	and the Morbus dev team
+]]
+
 local HBAP = 0
 local HBAY = 0
 local HBAR = 0
@@ -10,18 +13,16 @@ local HBPZ = 0
 local HEADBOB_ON = true
 
 function HeadBob(pl, pos, ang, fov)
-
-
 	local v = {}
+
 	v.pos = pos
 	v.ang = ang
 	v.fov = fov
-	
+
 	if pl:IsOnGround() && HEADBOB_ON then
 		if pl:KeyDown(IN_FORWARD) || pl:KeyDown(IN_BACK) || pl:KeyDown(IN_MOVELEFT) || pl:KeyDown(IN_MOVERIGHT) then
 			HBPZ = HBPZ + (10 / 1) * FrameTime()
 		end
-	
 		if pl:KeyDown(IN_FORWARD) then
 			if HBAP < 1.5 then
 				HBAP = HBAP + 0.05 * 1
@@ -31,7 +32,6 @@ function HeadBob(pl, pos, ang, fov)
 				HBAP = HBAP - 0.05 * 1
 			end
 		end
-		
 		if pl:KeyDown(IN_BACK) then
 			if HBAP > -1.5 then
 				HBAP = HBAP - 0.05 * 1
@@ -41,7 +41,6 @@ function HeadBob(pl, pos, ang, fov)
 				HBAP = HBAP + 0.05 * 1
 			end
 		end
-		
 		if pl:KeyDown(IN_MOVELEFT) then
 			if HBAR > -1.5 then
 				HBAR = HBAR - 0.07 * 1
@@ -51,7 +50,6 @@ function HeadBob(pl, pos, ang, fov)
 				HBAR = HBAR + 0.07 * 1
 			end
 		end
-		
 		if pl:KeyDown(IN_MOVERIGHT) then
 			if HBAR < 1.5 then
 				HBAR = HBAR + 0.07 * 1
@@ -68,9 +66,6 @@ function HeadBob(pl, pos, ang, fov)
 	v.ang.pitch = v.ang.pitch + HBAP * 1
 	v.ang.roll = v.ang.roll + HBAR * 1
 	v.pos.z = v.pos.z - math.cos(HBPZ * 1)
-	
-
-
 	return GAMEMODE:CalcView(pl, v.pos, v.ang, v.fov)
 end
 hook.Add("CalcView", "DasHeadBob", HeadBob)
--- morbusgame/gamemode/client/fx/cl_pp.lua
+++ morbusgame/gamemode/client/fx/cl_pp.lua
@@ -1,72 +1,70 @@
-// Morbus - morbus.remscar.com
-// Developed by Remscar
-// and the Morbus dev team
-
-
+--[[
+	Morbus - morbus.remscar.com
+	Developed by Remscar
+	and the Morbus dev team
+]]
 
 function ScreenEffects()
+	local ply = LocalPlayer()
 
-	if !Morbus.Blinded then	
+	if !ply.Fear then return end
+
+	if !Morbus.Blinded then
 		PostProcess()
     else
-    	Blind()
+		Blind()
     end
-
-    local ply = LocalPlayer()
-
-   	if !ply.Fear then return end
-
 	if ply.Fear > CurTime() then
-	  local f1 = (ply.Fear - CurTime())
-	  local f2 = ply.FearInt or 2
+		local f1 = (ply.Fear - CurTime())
+		local f2 = ply.FearInt or 2
 
-	  if ply.Fear-1 > CurTime() then
-	     local colorMod = {}
-	     colorMod["$pp_colour_addr"] = 0
-	     colorMod["$pp_colour_addg"] = 0
-	     colorMod["$pp_colour_addb"] = 0
-	     colorMod["$pp_colour_brightness"] = 0
-	     colorMod["$pp_colour_contrast"] = 0.1
-	     colorMod["$pp_colour_colour"] = 1
-	     colorMod["$pp_colour_mulr"] = 0
-	     colorMod["$pp_colour_mulg"] = 0
-	     colorMod["$pp_colour_mulb"] = 0
+		if ply.Fear-1 > CurTime() then
+			local colorMod = {}
 
-	     DrawColorModify(colorMod)
-	  end
+			colorMod["$pp_colour_addr"] = 0
+			colorMod["$pp_colour_addg"] = 0
+			colorMod["$pp_colour_addb"] = 0
+			colorMod["$pp_colour_brightness"] = 0
+			colorMod["$pp_colour_contrast"] = 0.1
+			colorMod["$pp_colour_colour"] = 1
+			colorMod["$pp_colour_mulr"] = 0
+			colorMod["$pp_colour_mulg"] = 0
+			colorMod["$pp_colour_mulb"] = 0
 
-
-	  DrawMotionBlur( 0.1, f1*f2, 0.05)
-
+			DrawColorModify(colorMod)
+		end
+		DrawMotionBlur( 0.1, f1*f2, 0.05)
 	end
-
 end
 hook.Add("RenderScreenspaceEffects","Stuff",ScreenEffects)
 
 function GetCloak(data)
-	local cloaktime = data:ReadFloat()
 	local ply = LocalPlayer()
+
 	if !ValidEntity(ply) then return end
+
+	local cloaktime = data:ReadFloat()
 	ply.Cloaked = true
 end
 usermessage.Hook("Send_Cloaking",GetCloak)
 
 function CancelCloak(data)
 	local ply = LocalPlayer()
+
 	if !ValidEntity(ply) then return end
+
 	ply.Cloaked = false
 end
 usermessage.Hook("Cancel_Cloaking",CancelCloak)
 
-
 function PostProcess()
 	if !LocalPlayer():Alive() then return end
 	if LocalPlayer():IsSwarm() then return end
+
 	local ply = LocalPlayer()
 	local amt = 0
-
-
 	local colorMod = {}
+
 	colorMod["$pp_colour_addr"] = 0
 	colorMod["$pp_colour_addg"] = 0
 	colorMod["$pp_colour_addb"] = 0
@@ -76,23 +74,23 @@ function PostProcess()
 	colorMod["$pp_colour_mulr"] = 0
 	colorMod["$pp_colour_mulg"] = 0
 	colorMod["$pp_colour_mulb"] = 0
-	
+
 	DrawColorModify(colorMod)
 	DrawBloom(0, 0.5, 2, 2, 1, 1, 1 - (LocalPlayer():Health() / 30), 1 - (LocalPlayer():Health() / 30), 1 - (LocalPlayer():Health() / 30))
 end
 
-
-
 function Blind()
 	local ColorModify = {}
-	ColorModify[ "$pp_colour_addr" ] 		= 0
-	ColorModify[ "$pp_colour_addg" ] 		= 0
-	ColorModify[ "$pp_colour_addb" ] 		= 0
-	ColorModify[ "$pp_colour_brightness" ] 	= 0
-	ColorModify[ "$pp_colour_contrast" ] 	= 0
-	ColorModify[ "$pp_colour_colour" ] 		= 0
-	ColorModify[ "$pp_colour_mulr" ] 		= 0
-	ColorModify[ "$pp_colour_mulg" ] 		= 0
-	ColorModify[ "$pp_colour_mulb" ] 		= 0
+
+	ColorModify[ "$pp_colour_addr" ] = 0
+	ColorModify[ "$pp_colour_addg" ] = 0
+	ColorModify[ "$pp_colour_addb" ] = 0
+	ColorModify[ "$pp_colour_brightness" ] = 0
+	ColorModify[ "$pp_colour_contrast" ] = 0
+	ColorModify[ "$pp_colour_colour" ] = 0
+	ColorModify[ "$pp_colour_mulr" ] = 0
+	ColorModify[ "$pp_colour_mulg" ] = 0
+	ColorModify[ "$pp_colour_mulb" ] = 0
+
 	DrawColorModify( ColorModify )
 end
